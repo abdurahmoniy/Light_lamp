c355ade67f604fafd2c1ec456196499f4f2b35c4
abdurahmoniy <96695070+abdurahmoniy@users.noreply.github.com>
2022-01-11 14:29:30 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let btn = document.getElementById('btn')
+let light =document.getElementById('light')
+
+function toggleBtn() {
+    btn.classList.toggle('active')
+    light.classList.toggle('on')
+}
